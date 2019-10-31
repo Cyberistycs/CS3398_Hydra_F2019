0258029914b46ae9cec8e6aed5e88a815a4a6c1f
Mack Scott <54640092+ms189@users.noreply.github.com>
2019-10-31 17:21:01 -0500

    Update Money_Channel_User_Login.js

--- Money_Channel_Wireframe/Money_Channel_User_Login.js
+++ Money_Channel_Wireframe/Money_Channel_User_Login.js
@@ -1,7 +1,23 @@
-function showNewUser() {
-    var button = document.getElementById()
-}
+function displayDate() {
 
-function showForgotPassword() {
-    
+    var date = new Date();
+
+    var mm = date.getMonth()+1;
+    var dd = date.getDate();
+    var yyyy = date.getFullYear();
+
+    if (mm < 10){
+        mm = '0' + mm;
+    }
+
+    if (dd < 10){
+        dd = '0' + dd;
+    }
+    var html = "";
+
+    html += "Welcome Back, Jane!<br>";
+    html +=  mm + '/' + dd + '/' + yyyy;
+    document.getElementById("showDate").innerHTML = html;
+
+    setTimeout(displayDate, 1000);
 }
